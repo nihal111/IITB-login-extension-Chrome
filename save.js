2733b7a3a0363b1c026995d273eb3025057a39f5
Arpan Banerjee <arpanbnrj9@gmail.com>
2016-01-06 01:32:08 +0530

    giving undefined object

--- save.js
+++ save.js
@@ -3,15 +3,12 @@
 function save_options() {
   var username = document.getElementsByTagName('input')[0].value;
   var password = document.getElementsByTagName('input')[1].value;
-  chrome.storage.sync.set({ "username" : username }, function() {
-    if (chrome.runtime.error) {
-      console.log("Runtime error.");
-    }
-  });
-    chrome.storage.sync.set({ "password" : password }, function() {
-    if (chrome.runtime.error) {
-      console.log("Runtime error.");
-    }
+  alert("SAVE!")
+chrome.storage.sync.set({
+    uname: username,
+    pass: password
+  }, function() {
+    console.log("Saved");
   });
 
 }
@@ -20,16 +17,14 @@ function save_options() {
 // stored in chrome.storage.
 function restore_options() {
   // Use default value color = 'red' and likesColor = true.
-  chrome.storage.sync.get("username", function(result) {
-    if (!chrome.runtime.error) {
-      console.log(result);
-      document.getElementsByTagName('input')[0].value=result;
-    }
-  });
-  chrome.storage.sync.get("password", function(result) {
-    if (!chrome.runtime.error) {
-      console.log(result);
-      document.getElementsByTagName('input')[1].value=result;
-    }
+  chrome.storage.sync.get({
+  }, function(items) {
+    console.log(items.uname)
+    console.log(items.pass)
+    document.getElementsByTagName('input')[0].value = items.uname;
+    document.getElementsByTagName('input')[1].value = items.pass;
   });
 }
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click',
+    save_options);
