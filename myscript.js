dffebbfb107cac7eeb9b7695b47946f792d6cb24
Arpan Banerjee <arpanbnrj9@gmail.com>
2016-01-06 04:23:06 +0530

    try n try till you...

--- myscript.js
+++ myscript.js
@@ -2,11 +2,14 @@ var form = document.forms[0];
 key1="username"
 key2="password"
   chrome.storage.local.get(key1 , function(items) {
-    console.log("Getting")
     form.getElementsByTagName('input')[0].value=items.username;
+    console.log(form.getElementsByTagName('input')[0].value)
   });
     chrome.storage.local.get(key2 , function(items) {
-    console.log("Getting")
+    console.log(items.password)
     form.getElementsByTagName('input')[1].value=items.password;
+    console.log(form.getElementsByTagName('input')[1].value)
   });
-form.submit();
+console.log("Going to submit")
+form.submit()
+console.log("Submitted")
