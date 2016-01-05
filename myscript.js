dab062a7cc7456f6fa0948ebf94de57ee2a3c90a
Nihal <nihal.111@gmail.com>
2016-01-06 04:18:42 +0530

    YAAAAA

--- myscript.js
+++ myscript.js
@@ -1,12 +1,30 @@
 var form = document.forms[0];
-key1="username"
-key2="password"
-  chrome.storage.local.get(key1 , function(items) {
-    console.log("Getting")
-    form.getElementsByTagName('input')[0].value=items.username;
-  });
-    chrome.storage.local.get(key2 , function(items) {
-    console.log("Getting")
-    form.getElementsByTagName('input')[1].value=items.password;
-  });
-form.submit();
+var username, password;
+
+function retrieve()
+{
+    key1="username"
+    key2="password"
+
+      chrome.storage.local.get(key1 , function(items) {
+        console.log("Getting")
+        username=items.username;
+        form.getElementsByTagName('input')[0].value=username;
+      });
+        chrome.storage.local.get(key2 , function(items) {
+        console.log("Getting")
+        password=items.password;
+        form.getElementsByTagName('input')[1].value=password;
+      });
+}
+
+function submit()
+{   console.log(username);
+    console.log(password);
+    console.log(form.getElementsByTagName('input')[0].value);
+    console.log(form.getElementsByTagName('input')[1].value);
+    form.submit();
+}
+
+retrieve();
+setTimeout(submit,1000);
--- save.js
+++ save.js
@@ -26,5 +26,5 @@ function restore_options() {
   });
 }
 
-//document.addEventListener('DOMContentLoaded', restore_options);
+document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click',save_options);
