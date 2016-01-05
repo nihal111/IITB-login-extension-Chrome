9e2cd14fd1e554f044475ee3dd9f6693b84309bd
Nihal <nihal.111@gmail.com>
2016-01-05 23:31:30 +0530

    Good Progress

---
+++ myscript.js
@@ -0,0 +1,6 @@
+var form = document.forms[0];
+name="150040015";
+password="npain!!!";
+form.getElementsByTagName('input')[0].value = name;
+form.getElementsByTagName('input')[1].value = password;
+form.submit();
