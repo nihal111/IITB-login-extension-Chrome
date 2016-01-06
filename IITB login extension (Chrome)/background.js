22233863b33155111f3baf7b4bb8cc3fc1bb01fb
Nihal <nihal.111@gmail.com>
2016-01-06 14:32:50 +0530

    Robusty

--- IITB login extension (Chrome)/background.js
+++ IITB login extension (Chrome)/background.js
@@ -1,6 +1,6 @@
 var tablink = "https://internet.iitb.ac.in/";
 var interval = 29*60*1000;
-
+var flag =0;
 function open(){
 	chrome.tabs.create({ url: tablink });
 	console.log("Opened internet.iitb.ac.in");
@@ -19,6 +19,14 @@ function perform(){
 	chrome.tabs.onUpdated.addListener(function(tabid, changeinfo, tab) {
     var url = tab.url;
         if (url == "https://internet.iitb.ac.in/logout.php" && changeinfo.status == "complete") {
+        	closetab();
+    }
+   });
+	chrome.tabs.onUpdated.addListener(function(tabid, changeinfo, tab) {
+    var url = tab.url;
+        if (url == "https://internet.iitb.ac.in/badpw.php" && changeinfo.status == "complete" && flag ==0) {
+        	alert("Invalid Username/password. Pleasee update extension configuration, by clicking the button near addressbar.");
+        	flag=1;
         	closetab();
     }
    });
