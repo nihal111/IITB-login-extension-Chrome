fbdd1b3c90d8564833bed60026ac4ee4e735d5f7
Nihal <nihal.111@gmail.com>
2016-01-08 01:58:33 +0530

    Added gpo and moodle with checkboxes

--- IITB login extension (Chrome)/myscript.js
+++ IITB login extension (Chrome)/myscript.js
@@ -1,29 +1,49 @@
 var form = document.forms[0];
 
-var username, password;
+var username, password,internet,gpo,moodle,website;
+
+function stringStartsWith (string, prefix) {
+    return string.slice(0, prefix.length) == prefix;
+}
 
 function retrieve()
 {
-    key1="username"
-    key2="password"
+    key1 = "username"
+    key2 = "password"
+    key3 = "internet"
+    key4 = "gpo"
+    key5 = "moodle"
 
       chrome.storage.local.get(key1 , function(items) {
-        console.log("Getting")
+        console.log("Getting username");
         username=items.username;
         form.getElementsByTagName('input')[0].value=username;
       });
         chrome.storage.local.get(key2 , function(items) {
-        console.log("Getting")
+        console.log("Getting passwoed");
         password=items.password;
         form.getElementsByTagName('input')[1].value=password;
       });
+        chrome.storage.local.get(key3 , function(items) {
+        internet=items.internet;
+        console.log(internet);
+
+      });
+        chrome.storage.local.get(key4 , function(items) {
+        gpo=items.gpo;
+        console.log(gpo);
+      });
+        chrome.storage.local.get(key5 , function(items) {
+        moodle=items.moodle;
+        console.log(moodle);
+      });
+        website = (window.location.href);
+
 }
 
 function submit()
-{   console.log(username);
-    console.log(password);
-    console.log(form.getElementsByTagName('input')[0].value);
-    console.log(form.getElementsByTagName('input')[1].value);
+{   
+    if ((stringStartsWith(website,"https://internet.iitb.ac.in") && internet==true) || (stringStartsWith(website,"https://gpo.iitb.ac.in/src/login.php") && gpo==true) || (stringStartsWith(website,"http://moodle.iitb.ac.in/login") && moodle==true))
     form.submit();
 }
 
--- IITB login extension (Chrome)/save.js
+++ IITB login extension (Chrome)/save.js
@@ -2,12 +2,18 @@
 var values = {}
 var key1 = "username"
 var key2 = "password"
+var key3 = "internet"
+var key4 = "gpo"
+var key5 = "moodle"
 
 function save_options() {
   var username = document.getElementsByTagName('input')[0].value;
   var password = document.getElementsByTagName('input')[1].value;
   values[key1]=username;
   values[key2]=password;
+  values[key3]=document.getElementById("internet").checked;
+  values[key4]=document.getElementById("gpo").checked;
+  values[key5]=document.getElementById("moodle").checked;
   chrome.storage.local.set(values);
 
 }
@@ -23,6 +29,18 @@ function restore_options() {
     chrome.storage.local.get(key2 , function(items) {
     console.log("Getting")
     document.getElementsByTagName('input')[1].value=items.password;
+  });
+    chrome.storage.local.get(key3 , function(items) {
+    console.log("Getting")
+    document.getElementById("internet").checked=items.internet;
+  });
+    chrome.storage.local.get(key4 , function(items) {
+    console.log("Getting")
+    document.getElementById("gpo").checked=items.gpo;
+  });
+    chrome.storage.local.get(key5 , function(items) {
+    console.log("Getting")
+    document.getElementById("moodle").checked=items.moodle;
   });
 }
 
